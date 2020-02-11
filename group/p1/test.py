3a6099f517d4d06068051a161f5c4b39cf95d05f
Raydrrrr1234 <ruikang.dai@gmail.com>
2020-02-11 15:38:00 +0800

    Backup

--- group/p1/test.py
+++ group/p1/test.py
@@ -1,5 +1,6 @@
 import cv2
 import numpy as np
+from numpy.linalg import inv,eig
 from scipy.ndimage.filters import convolve
 
 def gaussian_filter(sigma):
@@ -18,3 +19,77 @@ def generate_octave(init_level, s, sigma):
         next_level = convolve(octave[-1], kernel)
         octave.append(next_level)
     return octave
+
+cur = [[1,2,3], [2,3,4],[4,5,6]]
+s = 1
+sigma = 0.5
+octave = generate_octave(cur, s, sigma)
+
+def generate_gaussian_pyramid(im, num_octave, s, sigma):
+    pyr = []
+    for _ in range(num_octave):
+        octave = generate_octave(im, s, sigma)
+        pyr.append(octave)
+        im = octave[-3][::2, ::2]
+    return pyr
+
+def generate_DoG_octave(gaussian_octave):
+    octave = []
+    for i in range(1, len(gaussian_octave)):
+        octave.append(gaussian_octave[i]-gaussian_octave[i-1])
+    return np.concatenate([o[:,:,np.newaxis] for o in octave], axis=2)
+
+def generate_DoG_pyramid(gaussian_pyramid):
+    pyr = []
+    for gaussian_octave in gaussian_pyramid:
+        pyr.append(generate_DoG_octave(gaussian_octave))
+    return pyr
+
+def get_candidate_keypoints(D, w=16):
+    candidates = []
+    #D[:, :, 0] = 0
+    #D[:, :, -1] = 0
+    for i in range(w//2+1, D.shape[0]-w//2–1):
+        for j in range(w//2+1, D.shape[1]-w//2–1):
+            for k in range(1, D.shape[2]-1):
+                patch = D[i-1:i+2, j-1:j+2, k-1:k+2]
+                if np.argmax(patch) == 13 or np.argmin(patch) == 13:
+                    candidates.append([i, j, k])
+    return candidates
+
+def localize_keypoint(D, x, y, s):
+    dx = (D[y,x+1,s]-D[y,x-1,s])/2.
+    dy = (D[y+1,x,s]-D[y-1,x,s])/2.
+    ds = (D[y,x,s+1]-D[y,x,s-1])/2.
+    dxx = D[y,x+1,s]-2*D[y,x,s]+D[y,x-1,s]
+    dxy = ((D[y+1,x+1,s]-D[y+1,x-1,s]) — (D[y-1,x+1,s]-D[y-1,x-1,s]))/4.
+    dxs = ((D[y,x+1,s+1]-D[y,x-1,s+1]) — (D[y,x+1,s-1]-D[y,x-1,s-1]))/4.
+    dyy = D[y+1,x,s]-2*D[y,x,s]+D[y-1,x,s]
+    dys = ((D[y+1,x,s+1]-D[y-1,x,s+1]) — (D[y+1,x,s-1]-D[y-1,x,s-1]))/4.
+    dss = D[y,x,s+1]-2*D[y,x,s]+D[y,x,s-1]
+    J = np.array([dx, dy, ds])
+    HD = np.array([ [dxx, dxy, dxs], [dxy, dyy, dys], [dxs, dys, dss]])
+    offset = -inv(HD).dot(J)
+    return offset, J, HD[:2,:2], x, y, s
+
+def find_keypoints_for_DoG_octave(D, R_th, t_c, w):
+    candidates = get_candidate_keypoints(D, w)
+    keypoints = []
+    for i, cand in enumerate(candidates):
+        y, x, s = cand[0], cand[1], cand[2]
+        offset, J, H, x, y, s = localize_keypoint(D, x, y, s)
+        contrast = D[y,x,s] + .5*J.dot(offset)
+        if abs(contrast) < t_c: continue
+        w, v = eig(H)
+        r = w[1]/w[0]
+        R = (r+1)**2 / r
+        if R > R_th: continue
+        kp = np.array([x, y, s]) + offset
+        keypoints.append(kp)
+    return np.array(keypoints)
+
+def get_keypoints(DoG_pyr, R_th, t_c, w):
+    kps = []
+    for D in DoG_pyr:
+        kps.append(find_keypoints_for_DoG_octave(D, R_th, t_c, w))
+    return kps
