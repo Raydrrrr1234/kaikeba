4be6e5d47eae5c60147f68a60791f66e51787002
Raydrrrr1234 <ruikang.dai@gmail.com>
2020-02-12 14:52:45 +0800

    working on img_stitching

--- group/p1/image_stitching.py
+++ group/p1/image_stitching.py
@@ -11,34 +11,3 @@ import random
 import time
 import cv2
 
-img = cv2.imread("lenna.jpg", cv2.IMREAD_GRAYSCALE)
-sift = cv2.xfeatures2d.SIFT_create()
-surf = cv2.xfeatures2d.SURF_create()
-orb = cv2.ORB_create(nfeatures=1500)
-
-keypoints_sift, descriptors = sift.detectAndCompute(img, None)
-keypoints_surf, descriptors = surf.detectAndCompute(img, None)
-keypoints_orb, descriptors = orb.detectAndCompute(img, None)
-
-# Brute Force Matching
-'''
-bf = cv2.BFMatcher(cv2.NORM_HAMMING, crossCheck=True)
-matches = bf.match(des1, des2)
-matches = sorted(matches, key = lambda x:x.distance)
-matching_result = cv2.drawMatches(img1, kp1, img2, kp2, matches[:50], None, flags=2)
-'''
-
-img = cv2.drawKeypoints(img, keypoints_sift, None)
-cv2.imshow("Image", img)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
-
-img = cv2.drawKeypoints(img, keypoints_surf, None)
-cv2.imshow("Image", img)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
-
-img = cv2.drawKeypoints(img, keypoints_orb, None)
-cv2.imshow("Image", img)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
